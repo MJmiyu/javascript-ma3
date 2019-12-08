42848881cbedf0d59f488026ed2058e628307bd8
Marthe J. Neverdal <mjneverdal@outlook.com>
2019-12-08 20:23:52 +0100

    module assignment uploaded

--- level1/question1/script.js
+++ level1/question1/script.js
@@ -1,1 +1,39 @@
 //Use RegEx to validate form
+function formcheck(e) {
+  var isNumber = /^\d{8}$/;
+  var isEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,3}$/gi;
+  var phoneNumber = document.getElementById('phone-number').value;
+  var email = document.getElementById('email').value;
+  var lastName = document.getElementById('last-name').value;
+  var firstName = document.getElementById('first-name').value;
+  var error = false;
+
+  if (firstName.length > 0) {
+    console.log('Great Name');
+  } else {
+    console.log('You do not have a name?');
+    error = true;
+  }
+  if (lastName.length > 0) {
+    console.log('Good Name');
+  } else {
+    console.log('You do not have a name?');
+    error = true;
+  }
+
+  if (isNumber.test(phoneNumber)) {
+    console.log('The number is approved');
+  } else {
+    console.log('Not a valid phone number');
+    error = true;
+  }
+  if (isEmail.test(email)) {
+    console.log('Correct Email');
+  } else {
+    console.log('Incorrect Email address input');
+    error = true;
+  }
+  if (error) {
+    alert('Something is wrong here, check the console log too see what');
+  }
+}
--- level1/question2/script.js
+++ level1/question2/script.js
@@ -1,1 +1,33 @@
 //Console.log out elements in JSON file
+var json = `{
+	"video": [{
+		"id": 12312412312,
+		"name": "Ecuaciones Diferenciales",
+		"url": "/video/math/edo/12312412312",
+		"author": {
+			"data": [{
+				"name_author": "Alejandro Morales",
+				"url": "/author/alejandro-morales",
+				"type": "master"
+			}]
+		}
+	}]
+}
+`;
+
+var jsonData = JSON.parse(json);
+for (var i = 0; i < jsonData.video.length; i++) {
+  var video = jsonData.video[i];
+  console.log(video.id);
+  console.log(video.name);
+  console.log(video.url);
+  console.log(video.author);
+
+  var data = video.author.data;
+  for (var j = 0; j < data.length; j++) {
+    var author = data[j];
+    console.log(author.name_author);
+    console.log(author.url);
+    console.log(author.type);
+  }
+}
